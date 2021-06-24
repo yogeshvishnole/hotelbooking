5f297d0fbe172cd22d5000b52207b58219736ac0
yogeshvishnole <yogeshvishnole@gmail.com>
2021-06-24 09:08:13 +0530

    done

---
+++ server/routes/stripe.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const router = express.Router();
+
+// middleware
+import { requireSignin } from "../middlewares";
+// controllers
+import {
+  createConnectAccount,
+  getAccountStatus,
+  getAccountBalance,
+  stripeSessionId,
+  stripeSuccess,
+} from "../controllers/stripe";
+
+router.post("/create-connect-account", requireSignin, createConnectAccount);
+router.post("/get-account-status", requireSignin, getAccountStatus);
+router.post("/get-account-balance", requireSignin, getAccountBalance);
+router.post("/stripe-session-id", requireSignin, stripeSessionId);
+// order
+router.post("/stripe-success", requireSignin, stripeSuccess);
+
+module.exports = router;
